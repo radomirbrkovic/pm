d40507198004dd5a614969bb6a90610c39dd2ad0
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-01-23 20:31:32 +0100

    Update test - test_create_user_with_email_successful

--- app/user/tests/test_model.py
+++ app/user/tests/test_model.py
@@ -10,10 +10,15 @@ class UserModelTest(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful"""
+        data = {
+            'email': 'test@example.com',
+            'name': 'John Doe',
+            'password': 'test123'
+        }
         user = get_user_model().objects.create_user(
-            email="test@email.com",
-            password="test123"
+            **data
         )
 
-        self.assertEqual(user.email, "test@email.com")
-        self.assertTrue(user.check_password("test123"))
+        self.assertEqual(user.email, data['email'])
+        self.assertEqual(user.name, data['name'])
+        self.assertTrue(user.check_password(data['password']))
