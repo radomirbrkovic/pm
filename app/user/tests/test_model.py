c65dc5e40944ea9a00393f2b38098c26259123af
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-01-23 20:29:29 +0100

    User model

--- app/user/tests/test_model.py
+++ app/user/tests/test_model.py
@@ -10,7 +10,7 @@ class UserModelTest(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful"""
-        user = get_user_model().objects.create(
+        user = get_user_model().objects.create_user(
             email="test@email.com",
             password="test123"
         )
