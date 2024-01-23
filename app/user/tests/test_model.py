0c6359cdce8d02b1f8dc0313441c65140de75ca3
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-01-23 20:10:46 +0100

    UserModelTest - test_create_user_with_email_successful

---
+++ app/user/tests/test_model.py
@@ -0,0 +1,19 @@
+"""
+Test user model
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class UserModelTest(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful"""
+        user = get_user_model().objects.create(
+            email="test@email.com",
+            password="test123"
+        )
+
+        self.assertEqual(user.email, "test@email.com")
+        self.assertTrue(user.check_password("test123"))
