b03f65aec2976ed4df4d2fa7dae8775c61695c66
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-01-28 15:36:27 +0100

    Login - form

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 def index(request):
     return render(request, 'base.html')
+
+def login(request):
+    return  render(request, 'auth/login.html')
