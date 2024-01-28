74fd10f42aea723a9b30a9d329cd6946da66df41
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-01-28 15:56:56 +0100

    Login - authentication

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
-from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
 
 def index(request):
     return render(request, 'base.html')
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "Ypu are now login")
+            return redirect('index')
+        else:
+            messages.error(request, "Invalid credentials")
+            return redirect('login')
+
     return  render(request, 'auth/login.html')
