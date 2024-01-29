3d0829d3374148ef9e647ecbe1fbc8036b2b79c4
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-01-29 16:50:58 +0100

    Categories - model and migration

--- app/transaction/models.py
+++ app/transaction/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+class Category(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255, choices=(
+        ('revenue', "Revenue"),
+        ('asset', "Asset"),
+        ('liability', "Liability"),
+    ), default='revenue')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
