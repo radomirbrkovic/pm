9b3b9feec38ad84063c2b45af83ba169f8b6870d
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-01-31 21:19:39 +0100

    Index transactions template

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('user.urls')),
+    path('transactions/', include('transaction.urls')),
     path('admin/', admin.site.urls),
 ]
