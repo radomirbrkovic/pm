7aaf9857c2e76022851830aebf916a1386817d91
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-02-01 11:17:13 +0100

    Transactions - model and migration

--- app/transaction/models.py
+++ app/transaction/models.py
@@ -16,3 +16,18 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+    description = models.TextField(blank=True)
