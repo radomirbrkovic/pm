079d964333a1dddcd926475c183f006cec79ee3a
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-02-02 09:57:49 +0100

    Transactions - create

--- app/transaction/views.py
+++ app/transaction/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Transaction, Category
+from .validators import TransactionValidator
+from decimal import Decimal
 
 @login_required
 def index(request):
@@ -14,7 +16,27 @@ def index(request):
 
 def create(request):
     categories = Category.objects.filter(
-            user=request.user
-        ).order_by('name')
+        user=request.user
+    ).order_by('name')
     context = {'categories': categories}
+
+    if request.method == 'POST':
+        data = request.POST
+        validator = TransactionValidator()
+        if not validator.is_valid(request, data):
+            return redirect('transactions.create')
+
+        category = Category.objects.get(id=data['category'])
+
+        print(data)
+        Transaction.objects.create(
+            user=request.user,
+            category=category,
+            amount=Decimal(data['amount']),
+            date=data['date']
+        )
+
+        return redirect('transactions.index')
+
+
     return render(request, 'transactions/create.html', context)
