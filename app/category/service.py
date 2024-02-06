49bc36e3e532d72a4f06b20d6e269821c1cfbef6
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-02-06 14:52:33 +0100

    Categories - list with type names

--- app/category/service.py
+++ app/category/service.py
@@ -2,12 +2,13 @@ from .models import Category
 
 class CategoryService:
     """List of allowed types"""
-    types = (
-        ('income_active', "Income - active"),
-        ('income_passive', "Income - passive"),
-        ('asset', "Asset"),
-        ('liability', "Liability"),
-    )
+    types = {
+        'income_active':  "Income - active",
+        'income_passive': "Income - passive",
+        'asset': "Asset",
+        'liability': "Liability",
+    }
+
 
     def listOfCategories(self, request):
         categories = Category.objects.filter(
@@ -17,4 +18,9 @@ class CategoryService:
         if 'type' in request.GET and  request.GET['type'] is not "" :
             categories = categories.filter(type= request.GET['type'])
 
-        return categories.order_by('-id')
+        categories = categories.order_by('-id')
+
+        for category in categories:
+            category.type_name = self.types[category.type]
+
+        return categories
