c51b93d5d4e3227f9311e8a9227c7d54e18555a9
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-02-06 15:21:11 +0100

    Categories -  list with total transactions per category

--- app/category/service.py
+++ app/category/service.py
@@ -1,4 +1,5 @@
 from .models import Category
+from decimal import Decimal
 
 class CategoryService:
     """List of allowed types"""
@@ -18,9 +19,14 @@ class CategoryService:
         if 'type' in request.GET and  request.GET['type'] is not "" :
             categories = categories.filter(type= request.GET['type'])
 
-        categories = categories.order_by('-id')
+        categories = categories.order_by('-id').prefetch_related('transaction_set')
 
         for category in categories:
             category.type_name = self.types[category.type]
+            category.total_transaction = Decimal('0')
+
+            for transaction in category.transaction_set.all() :
+                category.total_transaction = category.total_transaction + transaction.amount
+
 
         return categories
