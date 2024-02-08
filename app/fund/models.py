6358d88dfe09bc2077724b4b985b18e2508ee764
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-02-08 16:38:22 +0100

    Fund - migration and model

--- app/fund/models.py
+++ app/fund/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+from category.models import Category
+class Fund(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+    initial_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    target_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    execution_date = models.DateField(auto_now_add=True)
+    description = models.TextField(blank=True)
 
-# Create your models here.
