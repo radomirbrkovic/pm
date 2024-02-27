80fc7d0833f90a4c9851ee5f1596f593ec2f800f
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-02-27 16:50:35 +0100

    Fund - model improvements

--- app/fund/models.py
+++ app/fund/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from category.models import Category
+from decimal import Decimal
 
 
 class Fund(models.Model):
@@ -16,3 +17,11 @@ class Fund(models.Model):
     target_amount = models.DecimalField(max_digits=10, decimal_places=2)
     execution_date = models.DateField(auto_now_add=True)
     description = models.TextField(blank=True)
+
+    def update(self, category, date, data):
+        self.category = category
+        self.execution_date = date
+        self.initial_amount = Decimal(data['initial_amount'])
+        self.target_amount = Decimal(data['target_amount'])
+        self.description = data['description']
+        self.save()
